a5eb10353768397fa20ffb34dbe3ebf4bedb1bba
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-04-23 18:11:11 -0700

    added get weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,10 +25,10 @@ function handleCityFormSubmit(event) {
     return;
   }
 
-  searchApi(cityInputVal);
+  searchApiCity(cityInputVal);
 }
 
-function searchApi(city) {
+function searchApiCity(city) {
   let cityURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apiKey;
 
   fetch(cityURL)
@@ -41,10 +41,14 @@ function searchApi(city) {
       }
     }) 
     .then(function (data) {
-      console.log(data[0].state);
+      
       if (data.length === 1) {
         // get weather for that city
-        window.location.href="./search.html?cityName="+data[0].name+"&lat="+data[0].lat + "&lon=" +data[0].lon;
+        if (data[0].state) {
+          window.location.href="./index.html?&lat="+data[0].lat + "&lon=" +data[0].lon+"&cityName="+data[0].name+"&state="+data[0].state+"&country="+data[0].country;
+        } else {
+          window.location.href="./index.html?&lat="+data[0].lat + "&lon=" +data[0].lon+"&cityName="+data[0].name+"&country="+data[0].country;
+        }
       } else {
         // Display list of possible cities
         displayCityList(data);
@@ -56,18 +60,24 @@ function searchApi(city) {
 function displayCityList(data) {
   // Display list of up to 5 possible cities to then search weather for
   for (let i=0; i<data.length; i++) {
-    let cityBox = $("<div>").addClass("row w-100");
+    let cityBox = $("<div>").addClass("row border border-info p-2 m-3");
     let cityDetails = $("<div>").addClass("col-12 col-md-6");
     let submitBox = $("<div>").addClass("col-12 col-md-6");
-    let submitButton = $("<button>").text("Get Weather");
+    let submitButton = $("<button>").addClass("btn btn-info").text("Get Weather");
     let cityName = data[i].name;
     let cityLat = data[i].lat;
     let cityLon = data[i].lon;
 
     if (data[i].state) {
       var cityString = cityName + ", " + data[i].state + ", " + data[i].country;
+      submitButton.click(function() {
+        window.location.href="./index.html?&lat="+cityLat + "&lon=" +cityLon+"&cityName="+cityName+"&state="+data[i].state+"&country="+data[i].country;;
+      })
     } else {
       var cityString = cityName + ", " + data[i].country;
+      submitButton.click(function() {
+        window.location.href="./index.html?&lat="+cityLat + "&lon=" +cityLon+"&cityName="+cityName+"&country="+data[i].country;
+      })
     }
     console.log(cityString);
 
@@ -75,9 +85,7 @@ function displayCityList(data) {
     cityDetails.append($("<p>").text("Lat: "+cityLat));
     cityDetails.append($("<p>").text("Lon: "+cityLon));
 
-    submitButton.click(function() {
-      window.location.href="./search.html?cityName="+cityName+"&lat="+cityLat + "&lon=" +cityLon;
-    })
+    
 
     submitBox.append(submitButton);
     resultsEl.append(cityBox.append(cityDetails).append(submitBox));
@@ -88,14 +96,37 @@ function displayCityList(data) {
 function displayWeather() {
   if (queryString) {
     queryArray = queryString.slice(1).split("&");
-    const locationData = {};
+    var locationData = {};
     for (i=0;i<queryArray.length;i++) {
       keyValuePair = queryArray[i].split("=");
       locationData[keyValuePair[0]] = keyValuePair[1];
     }
     console.log(locationData);
+    searchApiWeather("https://api.openweathermap.org/data/2.5/forecast?lat="+locationData.lat+"&lon="+locationData.lon+"&appid="+apiKey+"&units=imperial");
   }
 }
+
+function searchApiWeather(weatherURL) {
+  // Get weather data using latitude and longitude
+  fetch(weatherURL)
+    .then(function(response) {
+      console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .then(function (data) {
+      displayWeatherData(data);
+    }); 
+}
+
+function displayWeatherData(data) {
+  // Uses the data from the weather api to fill in the rest of the forecast
+  console.log(data);
+}
+
 
 //$.ajax({
 //  url: "/api/getWeather",
