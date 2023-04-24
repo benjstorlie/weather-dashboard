2723c93d387523e8f5cbdda6a6eb251ecf3fe65c
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-04-24 12:31:04 -0700

    added display weather, would like to change to empty hidden boxes in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,14 +19,14 @@ function init() {
 function handleCityFormSubmit(event) {
   event.preventDefault();
 
-  const cityInputVal = cityInputEl.val();
+  const cityInputVal = cityInputEl.val().trim();
 
   if (!cityInputVal) {
     console.error('You need a search input value!');
     return;
   }
 
-  searchApiCity(cityInputVal);
+  window.location.href="./index.html?cityName="+cityInputVal;
 }
 
 function searchApiCity(city) {
@@ -46,9 +46,9 @@ function searchApiCity(city) {
       if (data.length === 1) {
         // get weather for that city
         if (data[0].state) {
-          window.location.href="./index.html?&lat="+data[0].lat + "&lon=" +data[0].lon+"&cityName="+data[0].name+"&state="+data[0].state+"&country="+data[0].country;
+          window.location.href="./index.html?lat="+data[0].lat + "&lon=" +data[0].lon+"&cityName="+data[0].name+"&state="+data[0].state+"&country="+data[0].country;
         } else {
-          window.location.href="./index.html?&lat="+data[0].lat + "&lon=" +data[0].lon+"&cityName="+data[0].name+"&country="+data[0].country;
+          window.location.href="./index.html?lat="+data[0].lat + "&lon=" +data[0].lon+"&cityName="+data[0].name+"&country="+data[0].country;
         }
       } else {
         // Display list of possible cities
@@ -72,12 +72,12 @@ function displayCityList(data) {
     if (data[i].state) {
       var cityString = cityName + ", " + data[i].state + ", " + data[i].country;
       submitButton.click(function() {
-        window.location.href="./index.html?&lat="+cityLat + "&lon=" +cityLon+"&cityName="+cityName+"&state="+data[i].state+"&country="+data[i].country;;
+        window.location.href="./index.html?lat="+cityLat + "&lon=" +cityLon+"&cityName="+cityName+"&state="+data[i].state+"&country="+data[i].country;;
       })
     } else {
       var cityString = cityName + ", " + data[i].country;
       submitButton.click(function() {
-        window.location.href="./index.html?&lat="+cityLat + "&lon=" +cityLon+"&cityName="+cityName+"&country="+data[i].country;
+        window.location.href="./index.html?lat="+cityLat + "&lon=" +cityLon+"&cityName="+cityName+"&country="+data[i].country;
       })
     }
     console.log(cityString);
@@ -103,7 +103,22 @@ function displayWeather() {
       locationData[keyValuePair[0]] = keyValuePair[1];
     }
     console.log(locationData);
-    searchApiWeather("https://api.openweathermap.org/data/2.5/forecast?lat="+locationData.lat+"&lon="+locationData.lon+"&appid="+apiKey+"&units=imperial");
+    if (locationData.lat&&locationData.lon) {
+      let cityString = locationData.cityName + ', ';
+      if (locationData.state) {
+        cityString += locationData.state + ', ';
+      }
+      cityString += locationData.country;
+
+      resultsEl.append($('<div>').addClass('today-card container-fluid border').append($('<h2>').addClass('today-heading').text(cityString)));
+
+
+      searchApiWeather("https://api.openweathermap.org/data/2.5/forecast?lat="+locationData.lat+"&lon="+locationData.lon+"&appid="+apiKey+"&units=imperial");
+    } else if (locationData.cityName) {
+      searchApiCity(locationData.cityName);
+    } else {
+      resultsEl.append($("<h2>404 Not found. Try again!</h2>"));
+    }
   }
 }
 
@@ -123,15 +138,52 @@ function searchApiWeather(weatherURL) {
     }); 
 }
 
+function displayIcon() {
+  // Set the size of the Weather Icon in the header to the font-size of the header
+  $(".icon").append($("<img id='weather-icon' src='./assets/images/favicon/weather-transparent-favicon-16x16.svg'>").css("width",$("h1").css("line-height")));
+}
+
 function displayWeatherData(data) {
   // Uses the data from the weather api to fill in the rest of the forecast
   console.log(data);
+
 }
 
-function displayIcon() {
-  // Set the size of the Weather Icon in the header to the font-size of the header
-  $("#weather-icon").css("width",$("h1").css("line-height"));
+function displayTodaysWeather(dataTimestamp) {
+  // Fill in the card for the current weather, given that the location text is already in the heading
+  // Data timestamp is just one of the objects in the array data.list recieved from openweathermap
+
+  $(".today-heading").append($("<span>").text(dataTimestamp.dt_text));
+
+  let todayCard = $(".today-card");
+  todayCard.append($("<div>").addClass("today-weather-detail")
+    .text("Temperature: "+dataTimestamp.main.temp+"deg F"));
+  todayCard.append($("<div>").addClass("today-weather-detail")
+    .text("Wind: "+dataTimestamp.wind.speed+"MPH"));
+  todayCard.append($("<div>").addClass("today-weather-detail")
+    .text("Humidity: "+dataTimestamp.main.humidity+"%"));
 }
+
+function displayForecastDay(dataTimestamp) {
+  // Add a card into the forecast box containing weather for the given timestamp
+  // Data timestamp is just one of the objects in the array data.list recieved from openweathermap
+
+  let forecastCard=$("<div>").addClass("forecast-card");
+  // Add Heading
+  forecastCard.append($("<div>").addClass("forecast-card-heading h3")
+    .text(dataTimeStamp.dt_text));
+  forecastCard.append($("<div>").addClass("forecast-weather-detail")
+    .text("Temperature: "+dataTimestamp.main.temp+"deg F"));
+  forecastCard.append($("<div>").addClass("forecast-weather-detail")
+    .text("Wind: "+dataTimestamp.wind.speed+"MPH"));
+  forecastCard.append($("<div>").addClass("forecast-weather-detail")
+    .text("Humidity: "+dataTimestamp.main.humidity+"%"));
+}
+
+// I'd like to first sort the data by date, so I can get the high and low temps for a day, for example
+
+
+
 
 //$.ajax({
 //  url: "/api/getWeather",
